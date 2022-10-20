97f707d20d77fb54966b1df3625e38840a25a32f
alnnu <luannf1020@gmail.com>
2022-10-20 12:22:35 -0300

    definição dos candidatos e função para adicionar candidato

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,24 @@
-pragma solidity 0.5.16;
+pragma solidity ^0.5.16;
 
 contract Election {
     //read/write candidate
-    string public candidate;
+    struct Candidate{
+        uint id;
+        string  name;
+        uint voteCount;
+    }
 
-    constructor() public{
-        candidate = "Cadidate 1";
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidateCount;
+
+   constructor () public{
+       addCandidate("Lula");
+       addCandidate("Bolsonaro");
+    }
+
+    function addCandidate(string memory _name) private{
+        candidateCount ++;
+        candidates[candidateCount] = Candidate(candidateCount,_name, 0);
     }
 }
