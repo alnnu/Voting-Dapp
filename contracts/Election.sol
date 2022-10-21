43063003712ef593fd365928d3d634435a0e34ff
alnnu <luannf1020@gmail.com>
2022-10-20 22:10:03 -0300

    adição front end

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,15 +1,17 @@
 pragma solidity ^0.5.16;
 
 contract Election {
-    //read/write candidate
+    //define candidate
     struct Candidate{
         uint id;
         string  name;
         uint voteCount;
     }
 
+    //mapping and feching a Candidate
     mapping(uint => Candidate) public candidates;
 
+    //counting for the number of candidates
     uint public candidateCount;
 
    constructor () public{
@@ -17,6 +19,8 @@ contract Election {
        addCandidate("Bolsonaro");
     }
 
+
+    //add a candidate
     function addCandidate(string memory _name) private{
         candidateCount ++;
         candidates[candidateCount] = Candidate(candidateCount,_name, 0);
